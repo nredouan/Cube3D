eeb3303a411eb5e7a1c1664ff34941c4f4f289fb
Simon Cegla <scegla@1C3.42angouleme.fr>
2026-07-28 14:43:57 +0200

    un peu de norme

--- src/draw/draw.c
+++ src/draw/draw.c
@@ -6,7 +6,7 @@
 /*   By: scegla <scegla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 17:16:29 by nredouan          #+#    #+#             */
-/*   Updated: 2026/07/28 13:25:58 by scegla           ###   ########.fr       */
+/*   Updated: 2026/07/28 14:35:02 by scegla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,13 @@ void	draw_floor(t_game *game, int pixel, double x)
 
 void	draw_line(int tex_x, t_game *game, int line_height, double x)
 {
-	int	pixel;
-	int	draw_start;
-	int	draw_end;
+	int			pixel;
+	int			draw_start;
+	int			draw_end;
 	mlx_color	color;
-	
+	double		d;
+	int			tex_y;
+
 	draw_start = -line_height / 2 + HEIGHT / 2;
 	if (draw_start < 0)
 		draw_start = 0;
@@ -50,11 +52,11 @@ void	draw_line(int tex_x, t_game *game, int line_height, double x)
 	draw_ceiling(game, draw_start, x);
 	while (pixel < draw_end)
 	{
-		double d = pixel - HEIGHT / 2.0 + line_height / 2.0;
-    	int tex_y = (int)(d * game->so_height / line_height);
-    	if (tex_y < 0)
+		d = pixel - HEIGHT / 2.0 + line_height / 2.0;
+		tex_y = (int)(d * game->so_height / line_height);
+		if (tex_y < 0)
 			tex_y = 0;
-    	if (tex_y >= game->so_height)
+		if (tex_y >= game->so_height)
 			tex_y = game->so_height - 1;
 		color = mlx_get_image_pixel(game->mlx, game->so, tex_x, tex_y);
 		mlx_pixel_put(game->mlx, game->window, x, pixel, color);
@@ -87,29 +89,29 @@ void	draw_player(t_game *game)
 		game->player.pos_y * 12 - 2, 5, 5, green);
 }
 
-mlx_color get_color(int rgb[3])
+mlx_color	get_color(int rgb[3])
 {
-    mlx_color color;
+	mlx_color	color;
 
-    color.rgba = (rgb[0] << 24) | (rgb[1] << 16) | (rgb[2] << 8) | 0xFF;
-    return (color);
+	color.rgba = (rgb[0] << 24) | (rgb[1] << 16) | (rgb[2] << 8) | 0xFF;
+	return (color);
 }
 
 void	draw_walls(t_game *game, int side, double x)
 {
-	
 	int			line_height;
 	double		wallX;
+	int			tex_x;
 	// mlx_image	image;
 
 	game->color_c = get_color(game->c);
 	game->color_f = get_color(game->f);
 	if (side == 0)
-    	wallX = game->hit_y;
+		wallX = game->hit_y;
 	else
-    	wallX = game->hit_x;
+		wallX = game->hit_x;
 	wallX -= floor(wallX);
-	int tex_x = (int)(wallX * (double)game->so_width);
+	tex_x = (int)(wallX * (double)game->so_width);
 	line_height = HEIGHT / game->perp_wall_dist;
 	draw_line(tex_x, game, line_height, x);
 }
