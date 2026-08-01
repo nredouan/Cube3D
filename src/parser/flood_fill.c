8374d9ce2d2c655dc8a4616f5ddbf1479df04301
Nabil Redouane <nredouan@3A5.42angouleme.fr>
2026-08-01 17:22:52 +0200

    floodfill fixed

--- src/parser/flood_fill.c
+++ src/parser/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: nredouan <nredouan@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/01 16:50:29 by nredouan          #+#    #+#             */
-/*   Updated: 2026/08/01 17:04:49 by nredouan         ###   ########.fr       */
+/*   Updated: 2026/08/01 17:21:49 by nredouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,26 @@ static void	flood_fill(t_game *game, char **map, int y, int x)
 void	call_flood_fill(t_game *game)
 {
 	char	**map;
+	size_t	size;
+	int		i;
 
+	i = 0;
+	size = 0;
 	map = tab_char_cpy(game->map);
 	if (!map)
 	{
 		game_destroy(game);
+		exit(EXIT_FAILURE);
+	}
+	while (map[i])
+	{
+		size += ft_strlen(map[i]);
+		i++;
+	}
+	if (size > 100000)
+	{
+		game_destroy(game);
+		error_handler("The map is too big.");
 		exit(EXIT_FAILURE);
 	}
 	flood_fill(game, map, game->py, game->px);
